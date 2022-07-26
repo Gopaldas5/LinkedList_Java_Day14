bed3dba3ee01915aa886d1c940bf229f968e0927
GopalDas <gopalkrdas1997@gmail.com>
2022-07-26 05:53:11 +0530

    Added: Ability to insert between the number

--- src/com/bridgeLabz/MyLinkedList.java
+++ src/com/bridgeLabz/MyLinkedList.java
@@ -7,7 +7,6 @@ public class MyLinkedList<K> {
 
     public MyLinkedList() {
     }
-
     public void add(INode<K> node) {
         INode<K> tempNode = node;
         if (head == null) {
@@ -18,8 +17,30 @@ public class MyLinkedList<K> {
             tail = tempNode;
         }
     }
+    public void addFirst(INode<K> node) {
+        INode<K> tempNode = node;
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tempNode.setNext(head);
+            head = tempNode;
+        }
+    }
+    public void addMiddle(INode<K> node) {
+        INode<K> newNode = node;
+        INode<K> oldNode = node;
 
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            oldNode = head.getNext();
+            head.setNext(newNode);
+            newNode.setNext(oldNode);
+        }
+    }
     public void printList() {
-        System.out.println("MyNode " + head);
+        System.out.println("MyNode :- " + head);
     }
 }
